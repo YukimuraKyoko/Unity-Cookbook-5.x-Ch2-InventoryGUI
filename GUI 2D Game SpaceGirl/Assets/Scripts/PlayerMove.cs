bb1d61b5d66e3cf7f95948a8eeceaf939055fcc7
Tina Nguyen <TinaNguyen290@yahoo.com>
2018-10-22 13:57:24 -0700

    Deleted completed scene files, redid the PlayerMove Script.
    The complete scene files had too many same-name scripts so I decided to delete all of them and start from scratch.
    
    Re-did the PlayerMove Script

--- GUI 2D Game SpaceGirl/Assets/Scripts/PlayerMove.cs
+++ GUI 2D Game SpaceGirl/Assets/Scripts/PlayerMove.cs
@@ -1,40 +1,26 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-/*
- * basic 2D character controller
- * use array keys / WASD to move object up/down/left/right
- */
-public class PlayerMove : MonoBehaviour
-{
-	// change speed
-	public float spd = 10;
+public class PlayerMove : MonoBehaviour {
+    public float speed = 10;
+    private Rigidbody2D rdby2D;
 
-	// cached reference to a physics RigidBody
-	private Rigidbody2D rd2D;
-
-	//Initialize by obtain the current scripts attached object's Rigidbody2D
-	void Awake()
-	{
-		rd2D = GetComponent<Rigidbody2D>();
+	// Use this for initialization
+	void Awake () {
+        rdby2D = GetComponent<Rigidbody2D>();
 	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
 
-	void FixedUpdate()
-	{
-		// read from movement keys
-		// arrow keys / WASD
-		// GetAxis returns values between -1.0...0...+1.0
-		float xMove = Input.GetAxis("Horizontal");
-		float yMove = Input.GetAxis("Vertical");
+        float xSpeed = xMove * speed;
+        float ySpeed = yMove * speed;
 
-		// multiply by spd
-		float xSpeed = xMove * spd;
-		float ySpeed = yMove * spd;
+        Vector2 newVelocity = new Vector2(xSpeed, ySpeed);
 
-		// create (dx,dy) vector object
-		Vector2 newVelocity = new Vector2(xSpeed, ySpeed);
-
-		// set the velocity of the Physicsl rigid body to this (x,y) vector
-		rd2D.velocity = newVelocity;
+        rdby2D.velocity = newVelocity;
 	}
 }
